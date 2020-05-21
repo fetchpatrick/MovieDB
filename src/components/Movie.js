56aa29b979d128d00cf046cb750bb8c2c5388229
Patrick Alexander <patrick_alexander@mail.com>
2020-05-20 22:59:53 -0500

    movieInfoBar component complete

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -16,13 +16,21 @@ const Movie = ({ movieId }) => {
 	if (loading) {
 		return <Spinner />;
 	}
+
+	console.log(movie);
 	return (
 		<>
 			<Navigation movie={movie.original_title} />
 			<MovieInfo movie={movie} />
-			<MovieInfoBar />
-			<Grid>
-				<Actor />
+			<MovieInfoBar
+				time={movie.runtime}
+				budget={movie.budget}
+				revenue={movie.revenue}
+			/>
+			<Grid header='Actors'>
+				{movie.actors.map((actor) => (
+					<Actor key={actor.credit_id} actor={actor} />
+				))}
 			</Grid>
 		</>
 	);
